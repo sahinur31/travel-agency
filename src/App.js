40f6ab1f3c4ba92ed046b2d6a77a3f620164d324
NurulIslam-EEE <ni163034@gmail.com>
2022-01-26 05:46:56 +0600

    adding blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Blogs from './pages/home/blogs/Blogs';
+import AddBlog from './pages/Admin/AddBlog';
 
 function App() {
   return (
     <div className="App">
       <Blogs />
+      <AddBlog />
     </div>
   );
 }
