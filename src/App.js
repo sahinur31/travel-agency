0001e16a66719a34635cc9eb40a7e71c55166cd7
sahinur31 <sahinur4841@gmail.com>
2022-01-26 15:05:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import Blogs from './pages/home/blogs/Blogs';
-import AddBlog from './pages/Admin/AddBlog';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import AuthProvider from "./components/contexts/AuthProvider/AuthProvider";
+import Home from "./components/Pages/Home/Home/Home";
+import Login from "./components/Pages/Login/Login/Login";
+import Register from "./components/Pages/Login/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Blogs />
-      <AddBlog />
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      fontFamily:{
+        'title':['Raleway' , 'serif']
+      },
+      colors:{
+        'tomato':'#ff7c5b',
+        'dark-blue':'rgb(6,26,58)'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
